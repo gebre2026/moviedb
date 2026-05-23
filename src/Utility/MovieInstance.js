24692c71b4b25135203512c3ac39078402a2e285
gebre2026 <gebrejobfind@gmail.com>
2026-05-24 01:23:50 +0300

    fixed requestUrls import issue

--- src/Utility/MovieInstance.js
+++ src/Utility/MovieInstance.js
@@ -1,11 +1,17 @@
-import axios from "axios";
+// import axios from "axios";
 
-const movieInstance = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-});
+// const movieInstance = axios.create({
+//   baseURL: "https://api.themoviedb.org/3",
+// });
 
-export { movieInstance };
+// export { movieInstance };
 
 // https://api.themoviedb.org/3/discover/movie?api_key=b33da15fb726a97ac2874d63f7df0e4f&with_genres=99
 
+import axios from "axios";
 
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
+
+export default instance;
