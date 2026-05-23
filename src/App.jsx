7af8537c53809493376d6814eff25bf1ec4205af
gebre2026 <gebrejobfind@gmail.com>
2026-05-23 20:44:45 +0300

    fix footer import path

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import Banner from "./Components/Banner/Banner";
 import DisplayRow from "./Components/DisplayRow/DisplayRow";
-import Footer from "./components/Footer/Footer";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
